31be150bf77d1fcf51c1efd25afcdeb35bb0b056
Yohanskie101 <125958519+Yohanskie101@users.noreply.github.com>
2024-08-09 17:32:36 +0800

    New

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ import Projectthird from "../components/Projectthird";
 const Projects = () => {
   return (
     <div
-      className="container  w-full h-fit gap-7 mt-40 2xl:mt-56 xl:mt-56 lg:mt-44 md:mt-40 sm:mt-40  
+      className="container  w-full h-fit gap-7 mt-32 2xl:mt-56 xl:mt-56 lg:mt-44  sm:mt-32 
     "
       id="Proj-container"
     >
